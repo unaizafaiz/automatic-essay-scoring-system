df85e58fb8ef37a24df13de4e5b1848876bfb5da
Unaiza Faiz <unaizafaiz@Unaizas-MacBook-Pro.local>
2018-04-16 16:49:15 -0500

    score checking

--- src/main/java/com/nlp/autoscoring/criteria/Criteria.java
+++ src/main/java/com/nlp/autoscoring/criteria/Criteria.java
@@ -81,7 +81,7 @@ public class Criteria {
         SpellingChecker spellingChecker = new SpellingChecker();
         SentenceAgreement sentenceAgreement = new SentenceAgreement();
 
-        Scanner scanner = null;
+        /*Scanner scanner = null;
         HashMap<String, String> fileGrades = new HashMap<>();
 
         //reading essay file name and grade from the csv file and saving in a hash map
@@ -96,12 +96,12 @@ public class Criteria {
             String[] fileDetails = newLine.split(";");
             fileGrades.put(fileDetails[0],fileDetails[2]);
         }
-        scanner.close();
+        scanner.close();*/
 
         //getting average scores for all low and high essays in the training corpus
-        averageLow = Math.round(evaluatingTrainingSet(fileGrades, "low"));
-        averageHigh = Math.round(evaluatingTrainingSet(fileGrades, "high"));
-        trainingSet.close();
+      //  averageLow = Math.round(evaluatingTrainingSet(fileGrades, "low"));
+       // averageHigh = Math.round(evaluatingTrainingSet(fileGrades, "high"));
+       // trainingSet.close();
 
         float length;
         float scoreLength, scoreMistakes,scoreAgreement,scoreMissingVerb;
@@ -112,35 +112,35 @@ public class Criteria {
         int wordCount,sentenceCount;
 
        // System.out.println("Average - low: "+averageLow+" | high - "+averageHigh);
-        PrintWriter writer = null;
+        /*PrintWriter writer = null;
         try {
             writer = new PrintWriter("essaygrades.csv", "UTF-8");
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
-        }
+        }*/
 
         for(File file: files) {
             Preprocessing preprocessing = new Preprocessing();
             String fileContents = preprocessing.cleanFile(file);
-            length = (float) lengthOfEssay.findLengthOfEssay(fileContents);
-            scoreLength = scoreLength(length,averageHigh,averageLow);
+            length = (float) lengthOfEssay.findLengthOfEssay(fileContents); // 1
+           // scoreLength = scoreLength(length,averageHigh,averageLow);
             String[] mistake = spellingChecker.countSpellingMistakes(fileContents).split(" ");
-            spellingmistake = Integer.parseInt(mistake[0]);
-            wordCount = Integer.parseInt(mistake[1]);
+            spellingmistake = Integer.parseInt(mistake[0]); // 2
+           // wordCount = Integer.parseInt(mistake[1]);
             verbCounts = sentenceAgreement.countAgreementFailures(fileContents);
             String[] temp = verbCounts.split(" ");
-            agreement = Integer.parseInt(temp[0]);
-            missingVerb = Integer.parseInt(temp[1]);
-            sentenceCount = Integer.parseInt(temp[2]);
-            scoreMistakes = findScore(spellingmistake,wordCount,"b");
-            scoreAgreement = findScore(agreement,sentenceCount, "c1");
-            scoreMissingVerb = findScore(missingVerb,sentenceCount,"c2");
+            agreement = Integer.parseInt(temp[0]); // 3.1
+            missingVerb = Integer.parseInt(temp[1]); //3.2
+            //sentenceCount = Integer.parseInt(temp[2]);
+            //scoreMistakes = findScore(spellingmistake,wordCount,"b");
+           // scoreAgreement = findScore(agreement,sentenceCount, "c1");
+            //scoreMissingVerb = findScore(missingVerb,sentenceCount,"c2");
             String finalGrade = "L|H";
-            writer.println(file.getName()+";"+scoreLength+";"+scoreMistakes+";"+scoreAgreement+";"+scoreMissingVerb+";0;0;"+finalScore+";"+finalGrade);
+           // writer.println(file.getName()+";"+scoreLength+";"+scoreMistakes+";"+scoreAgreement+";"+scoreMissingVerb+";0;0;"+finalScore+";"+finalGrade);
         }
-         writer.close();
+        // writer.close();
     }
 
     private float scoreLength(float length, float averageHigh, float averageLow) {
@@ -161,8 +161,8 @@ public class Criteria {
         return score;
     }
 
-    public float findScore(int value, int total, String criteria){
-        float score= value/ (float) total;
+    public float findScore(int value, int min, int max, String criteria){
+        float score = (value - min)/ (float) (max-min);
        /* if(criteria.equals("b")){
 
         }
